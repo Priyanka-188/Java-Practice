3fef817b73e3566fce2e2f3f48a52aef9fd60c6e
Priyanka <sainipriyanka1722001@gmail.com>
2023-09-20 20:59:52 +0530

    display cube of a no.s upto a integer

---
+++ displayCube.java
@@ -0,0 +1,14 @@
+// Write a Java program to display the cube of the given number up to an integer.
+import java.util.Scanner;
+class displayCube{
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+
+        int n = sc.nextInt();
+
+        for(int i=1; i<=n; i++){
+           System.out.println("The cube of "+i+ " is " + (i*i*i));
+        }
+    }
+}
