5c90f4bbcb3d86d1f06ec58501d280d1f5780c85
Priyanka <sainipriyanka1722001@gmail.com>
2023-09-20 22:01:32 +0530

    rightAngledTrianlgePattern

--- rightAngledTrianlgePattern.java
+++ rightAngledTrianlgePattern.java
@@ -9,9 +9,9 @@ class rightAngledTrianlgePattern{
 
         for (int row =1; row<=n; row++){
             for(int col =1 ; col<=row; col++){
-                System.out.print(col);
+                System.out.print(col+" ");
             }
-            System.out.println();
+            System.out.println(" ");
         }
     }
 }
