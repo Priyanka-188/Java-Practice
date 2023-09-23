68f6d66ce4d1e218468c301cf0057f2c46a45b59
Priyanka <sainipriyanka1722001@gmail.com>
2023-09-23 22:14:48 +0530

    star trianlge pattern

---
+++ starTrnglePatern.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+/*Write a Java program to generate the following * triangles.
+
+Input the number: 6
+Expected Output :
+
+******                                                   
+ *****                                                   
+  ****                                                   
+   ***                                                   
+    **                                                   
+     *          */
+class starTrnglePatern{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter row number:");
+        int n = sc.nextInt();
+
+        for(int row=n; row>0; row--){
+            for(int spc=n-row; spc>0; spc--){
+                System.out.print(" ");
+            }
+            for(int col=1; col<=row ; col++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
