7f903e1e6c3558b86896ed3a4bbed84e44b5f77b
Priyanka <sainipriyanka1722001@gmail.com>
2023-10-28 21:17:42 +0530

    space triangle

---
+++ Patterns/seven.java
@@ -0,0 +1,21 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class seven {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of rows: ");
+        int n = sc.nextInt();
+
+        for(int row=1 ; row<=n; row++){
+            for(int spc = n-row; spc>=1 ; spc--){
+                System.out.print(" ");
+            }
+            for(int col =1; col<=2*row -1 ; col++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
