3c662dbdaef26337f6dc4edc2b4ff59b7f8c34e6
Priyanka <sainipriyanka1722001@gmail.com>
2023-10-29 17:43:27 +0530

    alphabet right angle triangle

---
+++ Patterns/twelve.java
@@ -0,0 +1,20 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class twelve {
+    public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter number of rows: ");
+    int n = sc.nextInt();
+
+    for(int row=1; row<=n ; row++){
+        char ch = 'A';
+        for(int col=1; col<=row; col++){
+            System.out.print(ch);
+            ch++;
+        }
+        System.out.println();
+    }
+}
+}
