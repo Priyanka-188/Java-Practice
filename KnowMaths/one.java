17dbab7e0534b5647547a14a2e954bea4aa46792
Priyanka <sainipriyanka1722001@gmail.com>
2023-10-31 20:59:41 +0530

    Count digits in a number

---
+++ KnowMaths/one.java
@@ -0,0 +1,21 @@
+package KnowMaths;
+import java.util.Scanner;
+
+public class one {
+
+    public static int countDigits(int n){
+        int count = 0;
+        while(n!= 0){
+            count++;
+            n = n/10;
+        }
+        return count;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number :");
+        int n = sc.nextInt();
+
+        System.out.println("The number of digits are: "+ countDigits(n));
+    }
+}
