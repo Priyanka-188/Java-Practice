d5c511d0e7e365bdb06262601b971a992c4af07e
Priyanka <sainipriyanka1722001@gmail.com>
2023-11-07 15:51:26 +0530

    Number is prime or not(Optimal approach)

--- KnowMaths/seven.java
+++ KnowMaths/seven.java
@@ -10,7 +10,7 @@ public class seven {
     public static boolean isPrime(int n){
         boolean isprime = true;
 
-        for(int i=2; i < n ; i++){
+        for(int i=2; i <= Math.sqrt(n) ; i++){
             if(n % i == 0){
                 isprime = false;
                 break;
